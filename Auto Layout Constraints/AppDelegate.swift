58ddabaed811e18b55322b6103116569c07bffa6
Ante Plecas <anteplecas@Antes-MacBook-Pro.local>
2020-02-14 14:02:59 -0600

    Added layout constraints

--- Auto Layout Constraints/AppDelegate.swift
+++ Auto Layout Constraints/AppDelegate.swift
@@ -13,7 +13,6 @@ import CoreData
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
@@ -70,8 +69,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             do {
                 try context.save()
             } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
                 let nserror = error as NSError
                 fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
             }
--- Auto Layout Constraints/ViewController.swift
+++ Auto Layout Constraints/ViewController.swift
@@ -10,9 +10,57 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var imageView1 = UIImageView()
+    var imageView2 = UIImageView()
+
+    var leadingConstraint: NSLayoutConstraint?
+    var trailingConstraint: NSLayoutConstraint?
+    var topConstraint: NSLayoutConstraint?
+    var bottomConstraint: NSLayoutConstraint?
+    
+    var leadingConstraint1: NSLayoutConstraint?
+    var trailingConstraint1: NSLayoutConstraint?
+    var topConstraint1: NSLayoutConstraint?
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        imageView1.image = UIImage(named: "larry david.jpg")
+        imageView2.image = UIImage(named: "larry david 1.jpg")
+
+        view.addSubview(imageView1)
+        view.addSubview(imageView2)
+        imageView1.contentMode = .scaleAspectFit
+        imageView2.contentMode = .scaleAspectFit
+        view.addSubview(imageView1)
+        view.addSubview(imageView2)
+
+        imageView1.translatesAutoresizingMaskIntoConstraints = false
+        imageView2.translatesAutoresizingMaskIntoConstraints = false
+
+        leadingConstraint = NSLayoutConstraint(item: imageView1, attribute: .leading, relatedBy: .equal, toItem: view, attribute: .leading, multiplier: 1.0, constant: 20.0)
+
+        trailingConstraint = NSLayoutConstraint(item: imageView1, attribute: .trailing, relatedBy: .equal, toItem: view, attribute: .trailing, multiplier: 1.0, constant: -20.0)
+
+        topConstraint = NSLayoutConstraint(item: imageView1, attribute: .top, relatedBy: .equal, toItem: view, attribute: .top, multiplier: 1.0, constant: 50.0)
+
+        bottomConstraint = NSLayoutConstraint(item: imageView1, attribute: .bottom, relatedBy: .equal, toItem: view, attribute: .bottom, multiplier: 1.0, constant: 0)
+        
+        leadingConstraint1 = NSLayoutConstraint(item: imageView2, attribute: .leading, relatedBy: .equal, toItem: imageView1, attribute: .leading, multiplier: 1.0, constant: 20.0)
+
+        trailingConstraint1 = NSLayoutConstraint(item: imageView2, attribute: .trailing, relatedBy: .equal, toItem: imageView1, attribute: .trailing, multiplier: 1.0, constant: -20.0)
+
+        topConstraint1 = NSLayoutConstraint(item: imageView2, attribute: .top, relatedBy: .equal, toItem: imageView1, attribute: .top, multiplier: 1.0, constant: -100)
+
+        leadingConstraint?.isActive = true
+        trailingConstraint?.isActive = true
+        topConstraint?.isActive = true
+        bottomConstraint?.isActive = true
+        
+        leadingConstraint1?.isActive = true
+        trailingConstraint1?.isActive = true
+        topConstraint1?.isActive = true
+
     }
 
 
