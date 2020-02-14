205e269fffccc08c03371543e906c442c82fa884
Ante Plecas <anteplecas@Antes-MacBook-Pro.local>
2020-02-14 00:55:37 -0600

    First commit

---
+++ Auto Layout Constraints/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Auto Layout Constraints
+//
+//  Created by Ante Plecas on 2/14/20.
+//  Copyright © 2020 Ante Plecas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Auto Layout ConstraintsTests/Auto_Layout_ConstraintsTests.swift
@@ -0,0 +1,34 @@
+//
+//  Auto_Layout_ConstraintsTests.swift
+//  Auto Layout ConstraintsTests
+//
+//  Created by Ante Plecas on 2/14/20.
+//  Copyright © 2020 Ante Plecas. All rights reserved.
+//
+
+import XCTest
+@testable import Auto_Layout_Constraints
+
+class Auto_Layout_ConstraintsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
